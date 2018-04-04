d590d4b038e9d0d906b16d60e616bda2b6cdf982
teng <zhang.ten@husky.neu.edu>
2018-04-03 22:58:57 -0400

    update

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 export PORT=5500
 
-cd ~/www/task_tracker3
-./bin/task_tracker3 stop || true
-./bin/task_tracker3 start
+cd ~/www/task_tracker
+./bin/task_tracker stop || true
+./bin/task_tracker start
 
